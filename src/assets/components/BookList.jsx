fefef141e4cf03b50823e42425c44acc45e41b9f
Donfallou <littlefallou@gmail.com>
2024-03-27 19:48:30 +0100

    implementazione Della homepage

---
+++ src/assets/components/BookList.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Search from "./Search";
+import Navbar from "./Navbar";
+function BookList(){
+    
+    return(
+       <>
+         <Navbar/>
+         <Search/>
+       </>
+
+    );
+}
+
+export default BookList;
